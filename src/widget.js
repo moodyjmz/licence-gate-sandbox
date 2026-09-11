61a1e6bfce5f824278c3d709e4157c355b908e5e
sandbox <noreply@github.com>
2026-09-11 11:09:35 +0200

    feat: update widget output
    Signed-off-by: sandbox <noreply@github.com>

--- src/widget.js
+++ src/widget.js
@@ -3,8 +3,10 @@
  *
  * Licensed under the Example License 1.0
  * You may obtain a copy at https://example.invalid/licence
+
+ * Modified by the Example project.
  */
 
 export function widget() {
-  return "widget";
+  return "widget v2";
 }
